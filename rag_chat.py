a3cad6a5747b2ec2e12d04df55cebb39752fbc28
Yashthakre-07 <neuroscience.devote@gmail.com>
2026-03-14 12:33:51 +0530

    Update rag_chat.py

--- rag_chat.py
+++ rag_chat.py
@@ -4,18 +4,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+api_key = os.getenv("GROQ_API_KEY")
 
+client = Groq(api_key=api_key)
 
 def ask_llm(prompt):
 
     completion = client.chat.completions.create(
         model="llama-3.1-8b-instant",
-        messages=[
-            {"role": "user", "content": prompt}
-        ],
+        messages=[{"role": "user", "content": prompt}],
         temperature=0.3
     )
-
 
     return completion.choices[0].message.content
